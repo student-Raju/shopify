cc3ef1d31546ff9d7c5dd13cf73211c644bb4537
student-Raju <rajgopalmahata@gmail.com>
2024-05-30 18:35:22 +0530

    shopify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import Homepage from './customer/pages/Homepage/Homepage';
 
 function App() {
   return (
-    <div className="App">
-     hello ecommerce
+    <div className="">
+    <Navigation/>
+    <div>
+      <Homepage/>
+    </div>
     </div>
   );
 }
---
+++ src/customer/pages/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+
+const Homepage = () => {
+  return (
+    <div><MainCarosel/>
+    <div>
+   other section
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
